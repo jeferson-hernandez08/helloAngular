d3cc5c3617cee70b031b2e4ecf72f068c346cbf9
Jeferson <jefer.hernandez1@gmail.com>
2026-02-12 14:15:24 -0500

    Continued heroesApp - db.json Database api

--- Angular/05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ Angular/05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
-  templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  templateUrl: './listado.component.html'
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  constructor( private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+
+    this.heroesService.getHeroes()
+      .subscribe( resp => console.log( resp ) );
   }
 
 }
